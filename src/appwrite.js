7a8b06ad313d621a19cb57aa28f3e6806cc040ba
Priyanshu Bhardwaj <Priyanshubhardwaj901@gmail.com>
2025-12-12 23:48:45 +0530

    Initial commit

--- src/appwrite.js
+++ src/appwrite.js
@@ -5,6 +5,8 @@ const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
+
+  client
   .setEndpoint('https://fra.cloud.appwrite.io/v1')
   .setProject(PROJECT_ID)
 
